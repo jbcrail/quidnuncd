36a0f82e0d4abeedb0bc06121d356a18a194c267
Joseph Crail <jbcrail@gmail.com>
2015-01-20 17:27:00 -0500

    Rename configuration structure.

--- src/qnd.c
+++ src/qnd.c
@@ -3,7 +3,7 @@
 /* Global data structure for server */
 struct qn_server server;
 
-struct qn_options {
+struct qn_config {
   char *host;
   char *port;
   int heartbeat_interval;
@@ -24,7 +24,7 @@ void usage()
   exit(EXIT_SUCCESS);
 }
 
-void parse_args(int argc, char **argv, struct qn_options *options)
+void parse_args(int argc, char **argv, struct qn_config *config)
 {
   for (int i = 1; i < argc; i++) {
     int lastarg = (i == argc-1);
@@ -34,11 +34,11 @@ void parse_args(int argc, char **argv, struct qn_options *options)
       printf(QND_VERSION "\n");
       exit(EXIT_SUCCESS);
     } else if (!strcasecmp(argv[i],"--host") && !lastarg) {
-      options->host = argv[++i];
+      config->host = argv[++i];
     } else if (!strcasecmp(argv[i],"--port") && !lastarg) {
-      options->port = argv[++i];
+      config->port = argv[++i];
     } else if (!strcasecmp(argv[i],"--heartbeat-interval") && !lastarg) {
-      options->heartbeat_interval = strtol(argv[++i], (char **)0, 10);
+      config->heartbeat_interval = strtol(argv[++i], (char **)0, 10);
     } else {
       fprintf(stderr, "Unknown option: '%s'\n", argv[i]);
       exit(EXIT_FAILURE);
@@ -48,13 +48,13 @@ void parse_args(int argc, char **argv, struct qn_options *options)
 
 int main(int argc, char **argv)
 {
-  // Define default options
-  struct qn_options options;
-  options.host = DEFAULT_HOST;
-  options.port = DEFAULT_PORT;
-  options.heartbeat_interval = DEFAULT_HEARTBEAT;
+  // Define default config
+  struct qn_config config;
+  config.host = DEFAULT_HOST;
+  config.port = DEFAULT_PORT;
+  config.heartbeat_interval = DEFAULT_HEARTBEAT;
 
-  parse_args(argc, argv, &options);
+  parse_args(argc, argv, &config);
 
   struct ev_signal signal_watcher;
   struct ev_io w_accept;
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
   struct ev_loop *loop = ev_default_loop(0);
 
   qn_server_init(&server);
-  if (qn_server_listen(&server, options.host, options.port) == -1) {
+  if (qn_server_listen(&server, config.host, config.port) == -1) {
     exit(EXIT_FAILURE);
   }
 
@@ -72,7 +72,7 @@ int main(int argc, char **argv)
   ev_io_init(&w_accept, accept_cb, server.sd, EV_READ);
   ev_io_start(loop, &w_accept);
 
-  ev_periodic_init(&heartbeat_timer, heartbeat_cb, 0.0, (ev_tstamp)options.heartbeat_interval, 0);
+  ev_periodic_init(&heartbeat_timer, heartbeat_cb, 0.0, (ev_tstamp)config.heartbeat_interval, 0);
   ev_periodic_start(loop, &heartbeat_timer);
 
   ev_loop(loop, 0);
