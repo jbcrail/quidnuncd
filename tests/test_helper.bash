a3b7e57500473ec83fb8dbd4d19b2342030ba490
Joseph Crail <jbcrail@gmail.com>
2015-01-28 22:52:59 -0500

    Fix Linux bug in assert_output_contains method.
    When testing on Linux, the above method would hang. I replaced the
    echo-grep combination with a builtin Bash primitive to check for a
    substring in the output.

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -57,7 +57,7 @@ assert_output_contains() {
     echo "assert_output_contains needs an argument" >&2
     return 1
   fi
-  echo "$output" | $(type -p ggrep grep | head -1) -F "$expected" >/dev/null || {
+  [[ "$output" = *"$expected"* ]] || {
     { echo "expected output to contain $expected"
       echo "actual: $output"
     } | flunk
